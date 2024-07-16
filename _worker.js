f5a95b282b0e415851f2869c4ba0ddcfb4cd34b8
mmt66 <56261475+mmt66@users.noreply.github.com>
2024-07-16 18:39:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '99010632-bdb0-4c0b-b847-5a0f95c5dfe5';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
